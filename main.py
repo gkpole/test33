4f84f2fe4b1593ff9e1d37f1308e8ab48aed1324
gkpole <89931423+gkpole@users.noreply.github.com>
2023-01-05 21:56:03 +0500

    Update main.py

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ async def zakaz(_:app, message: types.Message):
                         
 @app.on_message(filters.regex(r'one_month'))
 async def one_month(app, message):
-   answer = await app.ask(chat.id=message.chat_id, '✉️ | Введите вашу почту:')
+   answer = await app.ask(chat_id=message.chat_id, '✉️ | Введите вашу почту:')
    await answer.request.edit_text("Почта получена!")
    await answer.reply(f'Ваша почта: {answer.text}', quote=True)
 	
