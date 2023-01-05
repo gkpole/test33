81a2dbc7286c4dd22fb45ba751579c7ccee92c30
gkpole <89931423+gkpole@users.noreply.github.com>
2023-01-05 21:34:25 +0500

    Update main.py

--- main.py
+++ main.py
@@ -71,14 +71,14 @@ async def zakaz(_:app, message: types.Message):
 @app.on_message(filters.command("one_month"))
 async def one_month(_:app, message: types.Message):
     button = InlineKeyboardMarkup([[InlineKeyboardButton('Отменить', callback_data = 'stop')]])
-	question = await client.send_message(message.chat.id, '✉️ | Введите вашу почту.', reply_markup = button)
+    question = await app.send_message(message.chat.id, '✉️ | Введите вашу почту.', reply_markup = button)
 	# A nice flow of conversation
-	try:
-		response = await client.listen.Message(filters.text, id = filters.user(message.from_user.id), timeout=Noneq
-	if response:
-		await response.reply(f'Ваша почта: {response.text}')
-	else:
-		await message.reply('Okay cancelled question!')
+    try:
+	response = await app.listen.Message(filters.text, id = filters.user(message.from_user.id), timeout=Noneq
+    if response:
+	await response.reply(f'Ваша почта: {response.text}')
+    else:
+	await message.reply('Okay cancelled question!')
 	
 @app.on_callback_query()
 async def button(bot, update):
