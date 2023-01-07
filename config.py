e9972494fcc021225e7693dd343d7780691cd65a
gkpole <89931423+gkpole@users.noreply.github.com>
2023-01-07 22:28:05 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-TOKEN = '5764809787:AAHCXMbDqDclYanOYKnV62ls3HTbqyNS9XA'
-admin_user_id = -1001554634512 #юзер ид для логов, без скобочек!!!!
-sub_channel_id = -1001554634512  #ид канала для подписки
-sub_channel_url = "https://t.me/+QaNOTOF0xRE1YzFi" #ссылка на канал для подписки
-admin_id = 5893628848 #id для админ панели
+TOKEN = '5938393982:AAEYO7-Qt9IfR_CkFHu7wV2wRP3TNt09kxU'
+admin_user_id = -1001856841650 #юзер ид для логов, без скобочек!!!!
+sub_channel_id = -1001720706878 #ид канала для подписки
+sub_channel_url = "https://t.me/welat_vpn" #ссылка на канал для подписки
+admin_id = 5640383287 #id для админ панели
