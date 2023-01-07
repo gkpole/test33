60ae8398c79c90bcae8d638aca2404862bfef4af
gkpole <89931423+gkpole@users.noreply.github.com>
2023-01-07 18:49:26 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-TOKEN = '5485921311:AAFLCaqDtl-uTk7vowyR5-9kALFx-CuMf2E'
+TOKEN = '5485921311:AAHCRVMZqSyc5_eaSpmDa5B64689GHkAFo4'
 admin_user_id = -1001814890080 #юзер ид для логов, без скобочек!!!!
 sub_channel_id = -1001814890080  #ид канала для подписки
 sub_channel_url = "https://t.me/+k9n54y65zEVmOTFi" #ссылка на канал для подписки
